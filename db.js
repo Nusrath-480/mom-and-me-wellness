5989387733d42cd97f0093f36c0d0b01f68c5544
Nusrath-480 <nusrathbegum480@gmail.com>
2025-06-15 15:48:32 +0530

    db.js

---
+++ db.js
@@ -0,0 +1,14 @@
+function saveUserData(name, email, message) {
+  db.collection("contacts").add({
+    name: name,
+    email: email,
+    message: message,
+    timestamp: new Date()
+  })
+  .then(() => {
+    alert("Data saved successfully!");
+  })
+  .catch((error) => {
+    alert("Error saving data: " + error.message);
+  });
+}
